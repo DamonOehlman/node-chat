cb748c71793c2ca1c9c9c6d8c98ac5ad1cafe4d7
Damon Oehlman <damon.oehlman@sidelab.com>
2013-03-11 13:59:17 +1000

    Added additional load tests

--- test/manual/load-rooms.js
+++ test/manual/load-rooms.js
@@ -1,26 +1,32 @@
 var assert = require('assert'),
     chat = require('../../'),
-    room = chat.room(),
+    centralRoom = chat.room(),
     randomName = require('random-name'),
     uuid = require('uuid'),
     MuxDemux = require('mux-demux'),
     count = 1;
 
-function newConnection() {  
-    var connection = room.connect(),
+function newRoom() {  
+    var room = chat.room(),
+        stream = room.createStream();
+        connection = room.connect(),
         client = chat.client(connection, { nick: randomName().replace(/\s/g, '') });
 
+    // pipe the room into the central room
+    stream.pipe(centralRoom.createStream()).pipe(stream);
+
+    // incremen the room count
     count += 1;
 
     client.on('data', function handleMessage(msg) {
         if (msg.id === connection.id && msg.data === 'hello') {
             console.log('client created and message round-tripped, total clients = ' + count);
             room.removeListener('message', handleMessage);
-            newConnection();
+            newRoom();
         }
     });
 
     client.write('hello');
 }
 
-newConnection();
+newRoom();
